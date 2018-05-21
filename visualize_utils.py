1ff9684b9ba7bbdd672ec61375372ed6238cefff
= <=>
2018-05-21 16:48:01 +0900

    Edit visualize_utils.py clean up

--- visualize_utils.py
+++ visualize_utils.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
@@ -8,70 +6,44 @@ def my_imshow(array, interpolation='nearest', origin='bottom', aspect='auto', cm
   plt.imshow(array, interpolation=interpolation, origin=origin, aspect=aspect, cmap=cmap, **kwargs)
 
 
-def plot_piano_roll(piano_roll, plot_range=None, segment_len=None):
-  if not plot_range:
-    plot_range = [0, piano_roll.shape[0]]
-  my_imshow(piano_roll[plot_range[0]: plot_range[1]].T, vmin=0, vmax=1, cmap=plt.get_cmap('gray_r'))
-  x_ticks_sec = range(plot_range[0] // 100, plot_range[1] // 100)
-  plt.xticks([el * 100 for el in x_ticks_sec], x_ticks_sec)
-  octaves = range(piano_roll.shape[-1] // 12)
-  plt.yticks(octaves, [str(el + 21) for el in octaves])
-  plt.colorbar(ticks=[0, 1], pad=0.01, aspect=10)
-  if segment_len:
-    edges = range(segment_len * plot_range[0] // segment_len, segment_len * plot_range[1] // segment_len)
-    for el in edges:
-      plt.plot([el, el], [-0.5, piano_roll.shape[-1] - 0.5], color='red', linewidth=1, linestyle="--", alpha=0.8)
+note_names = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
 
-def plot_spectrogram(spectrogram, plot_range=None, segment_len=100):
-  if not plot_range:
-    plot_range = [0, spectrogram.shape[0]]
-  my_imshow(spectrogram[plot_range[0]: plot_range[1]].T, cmap=plt.get_cmap('gray_r'))
-  x_ticks_sec = range(plot_range[0] // 100, plot_range[1] // 100)
-  plt.xticks([el * 100 for el in x_ticks_sec], x_ticks_sec)
-  plt.colorbar(pad=0.01, aspect=10)
-  if segment_len:
-    edges = range(segment_len * plot_range[0] // segment_len, segment_len * plot_range[1] // segment_len)
-    for el in edges:
-      plt.plot([el, el], [-0.5, spectrogram.shape[-1] - 0.5], color='red', linewidth=1, linestyle="--", alpha=0.8)
+def midi_name(midi_num):
+  octave = str(midi_num // 12 - 1)
+  note = note_names[(midi_num + 3) % 12]
+  return octave, note
 
 
-def plot_train_pair(spec, pred, label, seg_len=None, title='', savename=None, max_len=1000):
-  plot_range = [0, np.min([spec.shape[0], max_len])]
-  plt.figure(figsize=(20, 10))
-  plt.subplot(311)
-  plt.title(title)
-  my_imshow(spec[plot_range[0]: plot_range[1]].T, cmap=plt.get_cmap('gray_r'))
-  x_ticks_sec = range(plot_range[0] // 100, plot_range[1] // 100)
-  plt.xticks([el * 100 for el in x_ticks_sec], x_ticks_sec)
-  plt.colorbar(pad=0.01, aspect=10)
-  if seg_len:
-    edges = range(plot_range[0], plot_range[1], seg_len)
-    for el in edges:
-      plt.plot([el, el], [0.5, spec.shape[-1] - 0.5], color='red', linewidth=1, linestyle="--", alpha=0.8)
-  plt.ylim([0, spec.shape[-1]])
-  plt.subplot(312)
+def draw_piano_roll(roll, draw_range=None, fps=10, midi_min=21, midi_max=108):
+  if not draw_range:
+    draw_range = [roll.shape[0], roll.shape[1]]
+  plt.imshow(roll[draw_range[0]: draw_range[1], :].T, interpolation='nearest', aspect='auto',
+             origin='lower', cmap=plt.get_cmap('gray_r'))
+  tick_range = range((draw_range[1] - draw_range[0]) // fps)
 
-  my_imshow(pred[plot_range[0]: plot_range[1]].T, vmin=0, vmax=1, cmap=plt.get_cmap('gray_r'))
-  x_ticks_sec = range(plot_range[0] // 100, plot_range[1] // 100)
-  plt.xticks([el * 100 for el in x_ticks_sec], x_ticks_sec)
-  plt.colorbar(ticks=[0, 1], pad=0.01, aspect=10)
-  if seg_len:
-    edges = range(plot_range[0], plot_range[1], seg_len)
-    for el in edges:
-      plt.plot([el, el], [0.5, pred.shape[-1] - 0.5], color='red', linewidth=1, linestyle="--", alpha=0.8)
-  plt.ylim([0, pred.shape[-1]])
-  plt.subplot(313)
-  my_imshow(label[plot_range[0]: plot_range[1]].T, vmin=0, vmax=1, cmap=plt.get_cmap('gray_r'))
-  x_ticks_sec = range(plot_range[0] // 100, plot_range[1] // 100)
-  plt.xticks([el * 100 for el in x_ticks_sec], x_ticks_sec)
-  plt.colorbar(ticks=[0, 1], pad=0.01, aspect=10)
-  if seg_len:
-    edges = range(plot_range[0], plot_range[1], seg_len)
-    for el in edges:
-      plt.plot([el, el], [0.5, label.shape[-1] - 0.5], color='red', linewidth=1, linestyle="--", alpha=0.8)
-  plt.ylim([0, label.shape[-1]])
-  if savename:
-    plt.savefig(savename)
-  plt.close()
+  # draw guide lines (octave line)
+  number = 12
+  cmap = plt.get_cmap('Paired')
+  colors = [cmap(i) for i in np.linspace(0, 1, number)]
 
+  n_midi = midi_max - midi_min + 1
+  edge = range(12 - midi_min % 12, n_midi, 12)
+  for el in edge:
+    plt.plot(draw_range, [el, el], color='red', linewidth=1, linestyle="--", alpha=0.8)
+  midi_ticks = [midi_name(el)[1] + midi_name(el)[0] for el in range(midi_min, midi_max)]
+  '''
+  # only shows white notes names
+  for n in range(len(midi_ticks)):
+    if '#' in midi_ticks[n]:
+      midi_ticks[n] = ''
+  '''
+
+  for n in range(n_midi):
+    plt.plot(draw_range, [n, n], color=colors[n % 12], linewidth=7, linestyle="-", alpha=0.07)
+  plt.yticks(range(n_midi), midi_ticks)
+  plt.ylim([0 - 0.5, n_midi - 0.5])
+
+  plt.xticks([el * fps for el in tick_range], [el + draw_range[0] for el in tick_range])
+  plt.xlabel('time(sec)')
+  plt.xlim(draw_range)
