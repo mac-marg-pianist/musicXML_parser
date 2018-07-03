0ce565044511dd4ce03f1b8a0adc67055d6a2709
Dasaem Jeong - iMac <jdasam@kaist.ac.kr>
2018-07-03 12:38:36 +0900

    test code cleaned

--- xml_midi_test.py
+++ xml_midi_test.py
@@ -3,14 +3,10 @@ import midi_utils.midi_utils as midi_utils
 import xml_matching
 
 
-# XMLDocument = MusicXMLDocument("magenta/testdata/chopin10-3/xml.xml")
-XMLDocument = MusicXMLDocument("magenta/testdata/chopinBallade1/xml.xml")
+XMLDocument = MusicXMLDocument("magenta/testdata/chopin10-3/xml.xml")
 melody_notes = xml_matching.extract_notes(XMLDocument, melody_only=False)
 melody_notes.sort(key=lambda x: x.note_duration.time_position)
-# midi = pretty_midi.PrettyMIDI("magenta/testdata/chopin10-3/midi.mid")
-# score_midi = midi_utils.to_midi_zero("magenta/testdata/chopin10-3/midi.mid")
-score_midi = midi_utils.to_midi_zero("magenta/testdata/chopinBallade1/midi.mid")
-
+score_midi = midi_utils.to_midi_zero("magenta/testdata/chopin10-3/midi.mid")
 perform_midi = midi_utils.to_midi_zero("magenta/testdata/chopin10-3/Sun08.mid")
 score_midi_notes = score_midi.instruments[0].notes
 perform_midi_notes = perform_midi.instruments[0].notes
@@ -19,28 +15,30 @@ corresp = xml_matching.read_corresp("magenta/testdata/chopin10-3/Sun08_infer_cor
 
 score_pairs, perform_pairs = xml_matching.match_xml_midi_perfrom(melody_notes,score_midi_notes, perform_midi_notes, corresp)
 
-# for pair in pairs:
-#     print('XML: ', pair['xml'].pitch, pair['xml'].note_duration.time_position, 'MIDI: ', pair['midi'])
-#
+
+# Check xml notes
+for i in range(len(melody_notes)-1):
+    # diff = (melody_notes[i+1].note_duration.time_position - melody_notes[i].note_duration.time_position) * 10000
+    # print(diff, melody_notes[i].note_duration.xml_position)
+    print(melody_notes[i].pitch, melody_notes[i].note_duration.xml_position,  melody_notes[i].note_duration.time_position)
+
+
+
+
+#Check score_pairs
 non_matched_count = 0
 for pair in score_pairs:
     if pair ==[]:
         non_matched_count += 1
+        print (pair)
     else:
         print('XML Note pitch:', pair['xml'].pitch , ' and time: ', pair['xml'].note_duration.time_position , '-- MIDI: ', pair['midi'])
 print('Number of non matched XML notes: ', non_matched_count)
-print(len(score_pairs), non_matched_count)
-# for note in melody_notes:
-    # print(note.note_duration.time_position, note.note_duration.xml_position)
-    # pass
-#
 
-# for i in range(len(melody_notes)-1):
-#     diff = (melody_notes[i+1].note_duration.time_position - melody_notes[i].note_duration.time_position) * 10000
-#     print(diff, melody_notes[i].note_duration.xml_position)
 
-# tempo_list = XMLDocument.get_tempos()
-# tempo_list = XMLDocument.recalculate_time_position()
-# for tempo in tempo_list:
-#     print(tempo, tempo.xml_position, tempo.state.divisions)
 
+
+features = xml_matching.extract_perform_features(melody_notes, perform_pairs)
+
+for feature in features:
+    print(feature)
