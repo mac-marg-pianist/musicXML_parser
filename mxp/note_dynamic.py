856696f365cab3e0f37b7ebd4493f4bbc60b6f8e
Dasaem Jeong - iMac <jdasam@kaist.ac.kr>
2018-07-18 17:09:47 +0900

    add Note Tempo class

--- mxp/note_dynamic.py
+++ mxp/note_dynamic.py
@@ -2,3 +2,9 @@ class NoteDynamic:
     def __init__(self):
         self.absoulte = None
         self.relative = []
+
+
+class NoteTempo:
+    def __init__(self):
+        self.absoulte = None
+        self.relative = []
